eedf0fdd48709fbfa9c7ca4a843f49ce9d9075b7
DarkPhoenix <phoenix@mail.ru>
2015-04-26 12:14:01 +0300

    Handle skill uniqueness using DB

--- service/data/pyfa_data/skill/core.py
+++ service/data/pyfa_data/skill/core.py
@@ -18,9 +18,8 @@
 #===============================================================================
 
 
-from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import Column, ForeignKey, Integer, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.orm.collections import attribute_mapped_collection
 
 from service.data.pyfa_data.base import PyfaBase
 from util.repr import make_repr_str
@@ -35,14 +34,13 @@ class Skill(PyfaBase):
 
     __tablename__ = 'skills'
 
-    id = Column('skill_id', Integer, primary_key=True)
-
-    _character_id = Column('character_id', Integer, ForeignKey('characters.character_id'), nullable=False)
+    _character_id = Column('character_id', Integer, ForeignKey('characters.character_id'), primary_key=True)
     _character = relationship('Character', backref=backref(
         'skills', collection_class=set, cascade='all, delete-orphan'))
 
-    eve_id = Column('type_id', Integer, nullable=False)
+    eve_id = Column('type_id', Integer, primary_key=True)
     level = Column(Integer, nullable=False)
+
 
     def __init__(self, type_id, level=0):
         self.eve_id = type_id
